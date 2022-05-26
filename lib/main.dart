7a0c63e12904635adc9316762063793011044f88
amilaindrajith <amila.indrajith.ukwattage@gmail.com>
2022-05-26 20:45:14 +0100

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:circle/pages/home_page.dart';
 import 'package:circle/pages/login.dart';
-import 'package:flutter/material.dart';
-
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
